e44da63faed16475b8dc917f1e52f327e029e1f4
YuraKochubailo <yurii.kochubailo@chisw.com>
2024-05-26 22:25:27 +0200

    add swagger

---
+++ src/main/java/com/serv/blog/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.serv.blog.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI API() {
+        return new OpenAPI()
+                .servers(
+                        List.of(
+                                new Server().url("http:localhost:8080")
+                        )
+                )
+                .info(
+                        new Info().title("Blog API")
+                );
+    }
+}
